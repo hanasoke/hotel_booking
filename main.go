d25065e5a8b3b8fc31fa70ecfb7b6576d5983192
hanasbp <hanasoke@gmail.com>
2026-01-18 12:00:47 +0700

    updated mainpage

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"hotel_booking/config"
+	"hotel_booking/controllers/homepage"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.ConnectDB()
+
+	// Homepage
+	http.HandleFunc("/", homepage.Index)
 
 	log.Println("Server running on port 2026")
 	http.ListenAndServe(":2026", nil)
