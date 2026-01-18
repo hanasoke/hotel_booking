cc49217a7723d5eb4790d9e39a2ee0dc34228703
hanasbp <hanasoke@gmail.com>
2026-01-18 12:46:43 +0700

    updated main pages

--- controllers/template.go
+++ controllers/template.go
@@ -1,1 +1,9 @@
 package controllers
+
+import "html/template"
+
+func LoadTemplate(files ...string) (*template.Template, error) {
+	base := []string{"views/templates/base.html"}
+	files = append(base, files...)
+	return template.ParseFiles(files...)
+}
