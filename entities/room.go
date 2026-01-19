ccbce9e4e7bab32f0092b401a718aa80df6511d2
hanasbp <hanasoke@gmail.com>
2026-01-19 16:14:39 +0700

    updated add.html

--- entities/room.go
+++ entities/room.go
@@ -1,1 +1,15 @@
 package entities
+
+import "time"
+
+type Room struct {
+	ID          int
+	Name        string
+	Type        string
+	Description string
+	Capacity    int
+	Status      string
+	Image       string
+	PricePerDay int
+	CreatedAt   time.Time
+}
--- models/roommodel/roommodel.go
+++ models/roommodel/roommodel.go
@@ -1,1 +1,28 @@
 package roommodel
+
+import (
+	"hotel_booking/config"
+	"hotel_booking/entities"
+)
+
+func Insert(room entities.Room) error {
+
+	query := `
+		INSERT INTO rooms
+		(name, type, description, capacity, status, image, price_per_day, created_at)
+		VALUES (?, ?, ?, ?, ?, ?, ?, NOW())
+	`
+
+	_, err := config.DB.Exec(
+		query,
+		room.Name,
+		room.Type,
+		room.Description,
+		room.Capacity,
+		room.Status,
+		room.Image,
+		room.PricePerDay,
+	)
+
+	return err
+}
