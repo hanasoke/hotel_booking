4dfe39b8fec6de18ad8f50cc99411f08ac4f9118
hanasbp <hanasoke@gmail.com>
2026-01-21 11:53:04 +0700

    updated booking.go

--- entities/booking.go
+++ entities/booking.go
@@ -1,1 +1,15 @@
 package entities
+
+import "time"
+
+type Booking struct {
+	ID                int
+	RoomID            Room
+	UserName          string
+	UserPhone         int
+	check_in_date     time.Time
+	check_out_date    time.Time
+	total_price       int
+	transaction_proof string
+	CreatedAt         time.Time
+}
