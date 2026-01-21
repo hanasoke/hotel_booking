3044fdc16dec9d3e76931f72b63560752344fd8b
hanasbp <hanasoke@gmail.com>
2026-01-21 21:07:48 +0700

    updated booking.go & add.html

--- entities/booking.go
+++ entities/booking.go
@@ -1,15 +1,29 @@
 package entities
 
-import "time"
+import (
+	"mime/multipart"
+	"time"
+)
 
 type Booking struct {
-	ID                int
-	RoomID            Room
-	UserName          string
-	UserPhone         int
-	check_in_date     time.Time
-	check_out_date    time.Time
-	total_price       int
-	transaction_proof string
-	CreatedAt         time.Time
+	ID               int
+	RoomID           int
+	Room             Room
+	UserName         string
+	UserPhone        string
+	CheckInDate      time.Time
+	CheckOutDate     time.Time
+	TotalPrice       int
+	TransactionProof string
+	CreatedAt        time.Time
+}
+
+type BookingForm struct {
+	RoomID           int
+	UserName         string
+	UserPhone        string
+	CheckInDate      string
+	CheckOutDate     string
+	TotalPrice       int
+	TransactionProof *multipart.FileHeader
 }
